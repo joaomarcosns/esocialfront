1b2e59d7c8f9ad4698cc0f36a4cc0b62f8bd94fb
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2022-08-08 14:59:55 -0300

    feat: implementando a chamada da aqui e criação das telas de login e de registro

---
+++ src/components/Auth/LoginComponents.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container-fluid vh-100">
+            <div class="" style="margin-top:200px">
+                <div class="rounded d-flex justify-content-center">
+                    <div class="col-md-4 col-sm-12 shadow-lg p-5 bg-light">
+                        <div class="text-center">
+                            <h3 class="text-primary">Login</h3>
+                        </div>
+                        <form action="">
+                            <div class="p-4">
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text bg-primary">
+                                        <i class="bi bi-person-plus-fill text-white"></i>
+                                    </span>
+                                    <input type="email" v-model="email" class="form-control" placeholder="E-mail">
+                                </div>
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text bg-primary">
+                                        <i class="bi bi-key-fill text-white"></i>
+                                    </span>
+                                    <input type="password" v-model="password" class="form-control" placeholder="Senha">
+                                </div>
+                                <button class="btn btn-primary text-center mt-2" @click="submit">
+                                    Entra
+                                </button>
+                                <p class="text-center mt-5">Voce não tem conta?
+                                    <a href="#" class="text-primary">Criar senha</a>
+                                </p>
+                                <a href="#" class="text-center text-primary">Esqueceu a senha?</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginComponents',
+}
+</script>
+
+<style>
+</style>
---
+++ src/views/Auth/CadastraView.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container-fluid vh-100">
+            <div class="" style="margin-top:200px">
+                <div class="rounded d-flex justify-content-center">
+                    <div class="col-md-4 col-sm-12 shadow-lg p-5 bg-light">
+                        <div class="text-center">
+                            <h3 class="text-primary">Castrar</h3>
+                        </div>
+                        <form action="">
+                            <div class="p-4">
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text bg-primary">
+                                        <i class="bi bi-person-plus-fill text-white"></i>
+                                    </span>
+                                    <input type="text" class="form-control" placeholder="Seu Nome">
+                                </div>
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text bg-primary">
+                                        <i class="bi bi-envelope text-white"></i>
+                                    </span>
+                                    <input type="email" class="form-control" placeholder="Seu E-mail">
+                                </div>
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text bg-primary">
+                                        <i class="bi bi-envelope text-white"></i>
+                                    </span>
+                                    <input type="email" class="form-control" placeholder="Confirmar E-mail">
+                                </div>
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text bg-primary">
+                                        <i class="bi bi-key-fill text-white"></i>
+                                    </span>
+                                    <input type="password" class="form-control" placeholder="Seu Senha">
+                                </div>
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text bg-primary">
+                                        <i class="bi bi-key-fill text-white"></i>
+                                    </span>
+                                    <input type="password" class="form-control" placeholder="Confirmar Senha">
+                                </div>
+                                <button class="btn btn-primary text-center mt-2" type="submit">
+                                    Entra
+                                </button>
+                                <p class="text-center mt-5">Voce já tem conta?
+                                    <a href="#" class="text-primary">Faz seu login</a>
+                                </p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+</template>
