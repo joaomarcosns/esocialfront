a1c71b2e92fa13a0fa64fcd23c5f1cd10ac827e5
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2022-08-08 23:32:44 -0300

    refactor: Arquivo da api

--- src/services/api.js
+++ src/services/api.js
@@ -2,11 +2,5 @@ import axios from 'axios'
 
 const api = axios.create({
     baseURL: 'http://127.0.0.1:8000/api/v1',
-    headers: {'Accept': 'application/json'}
 })
-api.defaults.headers['Accept'] = 'application/json'
-api.defaults.headers['Content-Type'] = 'application/json'
-api.defaults.headers.common['Authorization'] = `bearer ${localStorage.getItem('token')}`
-
-
 export default api
