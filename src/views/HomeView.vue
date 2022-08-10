a6239960473dc45d5c8dd5e46e19d1e7fa23ecf2
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2022-08-09 21:10:36 -0300

    feat: Iniciando criação da pagina home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,31 @@
 <template>
-  <div class="home">
-    <h1>Teste</h1>
+  <div class="rounded d-flex justify-content-center mt-5">
+    <div class="col-md-10 col-sm-12 shadow-lg p-5 bg-light">
+      <div class="text-center">
+        <h3 class="text-primary">Listagem dos Domínios</h3>
+      </div>
+      <div class="d-flex justify-content-end">
+        <button class="btn btn-primary">Importar <i class="fa-solid fa-file-import"></i></button>
+        <button class="btn btn-success ms-2">Exportar <i class="fa-solid fa-file-export"></i></button>
+      </div>
+      <div class="table-responsive mt-4">
+        <table class="table table-striped table-bordered">
+          <thead>
+            <tr >
+              <th class="text-center">Teste</th>
+              <th>Teste</th>
+              <th>Teste</th>
+              <th>Teste</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
-
-export default {
-}
+export default {};
 </script>
