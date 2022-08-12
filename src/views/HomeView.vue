e17ec3d8ba1dbfb93c222c963326fa8946557b90
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2022-08-12 08:46:40 -0300

    feat: Baixando modelo para importar os dados

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,13 +5,29 @@
         <h3 class="text-primary">Listagem dos Domínios</h3>
       </div>
       <div class="d-flex justify-content-end">
-        <button class="btn btn-primary">Importar <i class="fa-solid fa-file-import"></i></button>
-        <button class="btn btn-success ms-2">Exportar <i class="fa-solid fa-file-export"></i></button>
+        <div class="dropup">
+          <button
+            class="btn btn-secondary"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Importar <i class="fa-solid fa-file-import"></i>
+          </button>
+          <ul class="dropdown-menu">
+            <li><button class="dropdown-item" @click.prevent="modelImport">Baixar modelo</button></li>
+            <li><button class="dropdown-item">Importar dados</button></li>
+          </ul>
+        </div>
+
+        <button class="btn btn-success ms-2" @click.prevent="fileExport">
+          Exportar <i class="fa-solid fa-file-export"></i>
+        </button>
       </div>
       <div class="table-responsive mt-4">
         <table class="table table-striped table-bordered">
           <thead>
-            <tr >
+            <tr>
               <th class="text-center">Teste</th>
               <th>Teste</th>
               <th>Teste</th>
@@ -25,7 +41,57 @@
 </template>
 
 <script>
-// @ is an alias to /src
-
-export default {};
+import api from "@/services/api";
+import Cookie from "js-cookie";
+export default {
+  setup() {},
+  methods: {
+    fileExport() {
+      var headers = {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${Cookie.get("_myapp_token")}`,
+      };
+      api
+        .get("/domains/export", { headers: headers }, { ResponseType: "blob" })
+        .then((res) => {
+          var fileUrl = window.URL.createObjectURL(new Blob([res.data]));
+          var docUrl = document.createElement("a");
+          docUrl.href = fileUrl;
+          docUrl.setAttribute("download", "domains.csv");
+          document.body.appendChild(docUrl);
+          docUrl.click();
+        })
+        .catch((error) => {
+          var messageError = Object.entries(error.response.data.errors);
+          messageError.forEach((element) => {
+            alert(element[1]);
+          });
+        });
+    },
+    modelImport() {
+      var headers = {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${Cookie.get("_myapp_token")}`,
+      };
+      api
+        .get("/domains/modelImport", { headers: headers }, { ResponseType: "blob" })
+        .then((res) => {
+          var fileUrl = window.URL.createObjectURL(new Blob([res.data]));
+          var docUrl = document.createElement("a");
+          docUrl.href = fileUrl;
+          docUrl.setAttribute("download", "modelo.xlsx");
+          document.body.appendChild(docUrl);
+          docUrl.click();
+        })
+        .catch((error) => {
+          var messageError = Object.entries(error.response.data.errors);
+          messageError.forEach((element) => {
+            alert(element[1]);
+          });
+        });
+    },
+  },
+};
 </script>
