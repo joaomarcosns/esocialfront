daab46f8f276112592b887b1bda47db97a93627a
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2022-08-13 15:47:31 -0300

    feat: Fazer envio da planilha para importar os dados

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,8 +15,20 @@
             Importar <i class="fa-solid fa-file-import"></i>
           </button>
           <ul class="dropdown-menu">
-            <li><button class="dropdown-item" @click.prevent="modelImport">Baixar modelo</button></li>
-            <li><button class="dropdown-item">Importar dados</button></li>
+            <li>
+              <button class="dropdown-item" @click.prevent="modelImport">
+                Baixar modelo
+              </button>
+            </li>
+            <li>
+              <button
+                class="dropdown-item"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+              >
+                Importar dados
+              </button>
+            </li>
           </ul>
         </div>
 
@@ -38,13 +50,66 @@
       </div>
     </div>
   </div>
+
+  <!-- Modal -->
+  <div
+    class="modal fade"
+    id="exampleModal"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <form @submit.stop.prevent="importFile">
+          <div class="modal-body">
+            <div>
+              <input
+                @change.stop.prevent="file"
+                type="file"
+                name="file"
+                class="form-control"
+                id="file"
+                accept=".xlsx"
+              />
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Fechar
+            </button>
+            <button type="submit" class="btn btn-primary">
+              Importar dados
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import api from "@/services/api";
 import Cookie from "js-cookie";
 export default {
-  setup() {},
+  setup() {
+    return {
+      file: "",
+    };
+  },
   methods: {
     fileExport() {
       var headers = {
@@ -76,7 +141,11 @@ export default {
         Authorization: `Bearer ${Cookie.get("_myapp_token")}`,
       };
       api
-        .get("/domains/modelImport", { headers: headers }, { ResponseType: "blob" })
+        .get(
+          "/domains/modelImport",
+          { headers: headers },
+          { ResponseType: "blob" }
+        )
         .then((res) => {
           var fileUrl = window.URL.createObjectURL(new Blob([res.data]));
           var docUrl = document.createElement("a");
@@ -84,6 +153,31 @@ export default {
           docUrl.setAttribute("download", "modelo.xlsx");
           document.body.appendChild(docUrl);
           docUrl.click();
+        })
+        .catch((error) => {
+          var messageError = Object.entries(error.response.data.errors);
+          messageError.forEach((element) => {
+            alert(element[1]);
+          });
+        });
+    },
+    file(event) {
+      this.file = event.target.files[0];
+    },
+    importFile() {
+      var form = "";
+      form = new FormData();
+      form.append("file", this.file);
+      var headers = {
+        "Content-Type": "multipart/form-data",
+        Accept: "application/json",
+        Authorization: `Bearer ${Cookie.get("_myapp_token")}`,
+      };
+
+      api
+        .post("/domains/upload", form, { headers: headers })
+        .then((response) => {
+          alert(response.data.message);
         })
         .catch((error) => {
           var messageError = Object.entries(error.response.data.errors);
